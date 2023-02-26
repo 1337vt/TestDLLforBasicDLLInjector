a593bfabb29136e32b0c24f5681f6f9924fa170b
clayton <clayton@CLAYDESKTOP>
2023-02-25 21:10:36 -0600

    Add project files.

---
+++ ConsolePopUp/dllmain.cpp
@@ -0,0 +1,24 @@
+#include "pch.h"
+#define EXPORT extern "C" __declspec(dllexport)
+
+EXPORT void HelloWorld()
+{
+    MessageBox(0, L"Process Injected!", 0, 0);
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        HelloWorld();
+        break;
+    case DLL_THREAD_ATTACH:
+        break;
+    case DLL_THREAD_DETACH:
+        break;
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
